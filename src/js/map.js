44f98a7b3e92caf75a81d20276fb1692a5e890e7
Alexey Istomin <webistomin@gmail.com>
2017-08-26 16:43:41 +0300

    Оживил жсом :smile:

--- src/js/map.js
+++ src/js/map.js
@@ -1,21 +1,35 @@
-var mapOpen = document.querySelector(".js-open-map");
-var mapPopup = document.querySelector(".modal-content-map");
-var mapClose = mapPopup.querySelector(".modal-content-close");
+var Maplink = document.querySelector(".js-open-map");
+var Mappopup = document.querySelector(".map-popup");
+var Mapclose = Mappopup.querySelector(".close-btn");
+var Mapoverlay = document.querySelector(".overlay");
 
-mapOpen.addEventListener("click", function(event) {
-    event.preventDefault();
-    mapPopup.classList.add("modal-content-show");
+Maplink.addEventListener("click", function(event) {
+  event.preventDefault();
+  Mappopup.classList.add("map-popup--show");
+  Mapoverlay.classList.add("overlay--show");
 });
 
-mapClose.addEventListener("click", function(event) {
-    event.preventDefault();
-    mapPopup.classList.remove("modal-content-show");
+Mapclose.addEventListener("click", function(event) {
+  event.preventDefault();
+  Mappopup.classList.remove("map-popup--show");
+  Mapoverlay.classList.remove("overlay--show");
+});
+
+Mapoverlay.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (Mapoverlay.classList.contains("overlay--show")) {
+    Mapoverlay.classList.remove("overlay--show");
+  }
+  if (Mappopup.classList.contains("map-popup--show")) {
+    Mappopup.classList.remove("map-popup--show");
+  }
 });
 
 window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (mapPopup.classList.contains("modal-content-show")) {
-            mapPopup.classList.remove("modal-content-show")
-        }
+  if (event.keyCode === 27) {
+    if (Mappopup.classList.contains("map-popup--show")) {
+      Mappopup.classList.remove("map-popup--show");
+      Mapoverlay.classList.remove("overlay--show");
     }
+  }
 });
